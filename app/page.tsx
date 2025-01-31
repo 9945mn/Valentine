da48bb2971acec6438f121307e9a4826c829ee6f
9945mn <9945mn@gmail.com>
2025-01-31 15:05:35 -0600

    Fix routing

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/app/ValentineProposal.tsx"
+            // className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/valentine"
           >
             Go to Valentine’s Day Page
           </Link>
